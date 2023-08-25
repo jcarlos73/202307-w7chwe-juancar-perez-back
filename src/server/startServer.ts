cee0a1df92b7d5c300e2050b657e2a93c06ad8fb
Juan Carlos Pérez <jcparetas@gmail.com>
2023-08-25 13:19:28 +0200

    Initial commit

---
+++ src/server/startServer.ts
@@ -0,0 +1,14 @@
+import chalk from "chalk";
+import app from "./index.js";
+
+const startServer = () => {
+  const port = 4000;
+
+  app.listen(port, () => {
+    console.log(
+      chalk.blue(`Listening on ${chalk.bold(`http://localhost:${port}`)}`),
+    );
+  });
+};
+
+export default startServer;
