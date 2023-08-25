a459ab4bb211510113b504a36bea3406b0745141
Juan Carlos Pérez <jcparetas@gmail.com>
2023-08-25 19:44:37 +0200

    Create robotsRouter.ts to folder routers and add imports

---
+++ src/server/controllers/routers/robotsRouter.ts
@@ -0,0 +1,5 @@
+import express from "express";
+
+const robotsRouter = express.Router();
+
+export default robotsRouter;
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,10 @@
 import express from "express";
+import morgan from "morgan";
+import { pingController } from "./controllers/ping/pingController";
 const app = express();
+
+app.use(morgan("dev"));
+
+app.use("/", pingController);
 
 export default app;
